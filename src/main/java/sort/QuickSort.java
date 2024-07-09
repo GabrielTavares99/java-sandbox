56a3d23d8d7715f9b44ddc528717a043e03c593b
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2024-07-09 17:19:40 -0300

    random pivot to quick sort

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -4,17 +4,19 @@ import lombok.var;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class QuickSort {
 
+    private Random random = new Random();
 
     public List<Integer> sort(List<Integer> list) {
 
         if (list.size() < 2)
             return list;
 
-        var pivot = list.remove(0);
+        var pivot = list.remove(random.nextInt(list.size()));
 
         final List<Integer> lowers = list.stream().filter(e -> e < pivot).collect(Collectors.toList());
         final List<Integer> uppers = list.stream().filter(e -> e > pivot).collect(Collectors.toList());
